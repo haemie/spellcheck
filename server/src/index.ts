ef83fc31c4d9c4d7ab5eaed10dc86b04faf93a04
haemie <jmezhang@gmail.com>
2024-03-13 16:43:49 -0500

    added nodemon, dotenv

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server listening on ${PORT}`));
