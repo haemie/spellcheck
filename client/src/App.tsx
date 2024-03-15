0743a892bf90d9cd3581e4b4f707f17600eccde1
haemie <jmezhang@gmail.com>
2024-03-15 16:23:24 -0500

    completed post functionality for words to check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,12 +7,29 @@ function App() {
   const [userForm, setUserForm] = useState('');
   const [wordForm, setWordForm] = useState('');
   const [targetWord, setTargetWord] = useState('');
+  const divRef: React.RefObject<HTMLDivElement> = useRef(null);
 
   async function submitWord(e: FormEvent) {
     e.preventDefault();
-    const response = await fetch('http://localhost:8000/game/checkWord');
-    const word = await response.json();
-    console.log('submitted');
+    try {
+      const response = await fetch('http://localhost:8000/game/checkWord', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ submittedWord: wordForm }),
+      });
+      const result = await response.json();
+      if (divRef.current) {
+        document.body.style.backgroundColor = result;
+        setTimeout(() => {
+          document.body.style.backgroundColor = 'inherit';
+        }, 500);
+      }
+      console.log('submitted');
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   async function getWord(e: FormEvent) {
@@ -22,28 +39,42 @@ function App() {
       const word = await response.json();
       setTargetWord(word);
       console.log('setted word from server');
+      // now read word?
+      const msg = new SpeechSynthesisUtterance();
+      msg.text = targetWord;
+      window.speechSynthesis.speak(msg);
     } catch (err) {
       console.error('failed to get word from server');
     }
   }
 
+  function playbackWord() {
+    const msg = new SpeechSynthesisUtterance();
+    msg.text = targetWord;
+    window.speechSynthesis.speak(msg);
+  }
+
   useEffect(() => {
-    console.log(wordForm);
+    // console.log(wordForm);
   });
 
   return (
     <>
       <h1>spellcheck</h1>
-      <div>
+      <div ref={divRef}>
         {targetWord ? (
-          <form onSubmit={submitWord}>
-            <input
-              type="text"
-              value={wordForm}
-              onChange={(e) => setWordForm(e.target.value)}
-            />
-            <input type="submit" value={'submit'} />
-          </form>
+          <>
+            {' '}
+            <form onSubmit={submitWord}>
+              <input
+                type="text"
+                value={wordForm}
+                onChange={(e) => setWordForm(e.target.value)}
+              />
+              <input type="submit" value={'submit'} />
+            </form>
+            <input type="button" onClick={playbackWord} value="play word" />
+          </>
         ) : (
           <input type="button" onClick={getWord} value="new word" />
         )}
